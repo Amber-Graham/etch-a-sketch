27926fe8b90b2bb7b9fe3459fe8cc63cec66f642
Amber-Graham <ang190045@gmail.com>
2022-07-20 12:15:35 -0500

    write psuedocode

---
+++ javascript.js
@@ -0,0 +1,31 @@
+// Things we will need that the assignment is already telling us:
+// create a webpage with 16x16 grid of square divs
+// divs will be created using JS 
+// put the square divs inside a large "container" div
+// the container div will go directly into the HTML
+// keep in mind: margins and borders may affect the square divs
+// set up a hover effect so that the grid divs change color when 
+    // your mouse passes over them, leaving a pixelated trail through
+    // your grid like a pen would
+// "Hovering" is what happens when your mouse enters a div and ends 
+    // when your mouse leaves it. set up event listeners for these 
+// add a class to the div to change the color (maybe use classList.add?)
+// add a button to the top of the screen
+    // users will be prompted to enter in a number that represents
+    // the number of squares per side for the new grid
+// once the number is entered, the existing grid should be removed 
+    // and a new one should be generated in the same total space as before
+    // so you have a new sketch pad. make sure to set the limit for the user 
+    // input to a mex of 100 
+// research button tags in html and how you can make a JS function 
+    //run when one is clicked
+// if a user enters 64, they should have a brand new 64x64 grid
+    // pop up without changing the total amount of pixels used
+
+// basically: there will be an interface that lives at the top of the page
+    // this interface will have the etch-a-sketch title
+    // it will have a section for the user to adjust the grid
+    // it will have the color mode option (bonus points only)
+    // it will have a reset option
+    // when the mouse goes over the grid, it will color the square to black
+        // or a random RGB color
