95a8e501f8f5270b0ff557e9fada06e5d63228cc
Amber-Graham <ang190045@gmail.com>
2022-07-21 12:21:09 -0500

    add functions to pick color and size

--- javascript.js
+++ javascript.js
@@ -9,33 +9,64 @@
 // "Hovering" is what happens when your mouse enters a div and ends 
     // when your mouse leaves it. set up event listeners for these 
 // add a class to the div to change the color (maybe use classList.add?)
-// add a slider to the top of the screen to go all the way up to 50 x 50 grid
-// if the slider is adjusted, the grid resets
 // there will also be a reset button
 // research button tags in html and how you can make a JS function 
     //run when one is clicked
 
-// things i KNOW i need
 
-const container = document.querySelector('#container');
-const slider = document.getElementById('gridRange');
-const output = document.getElementById('gridOption');
-output.innerHTML = slider.value;
-
-// i will need a const for the square divs
-// a formula to change the grid size according to the slider
-// a function to call the grid size
 // a function to create the grid
-// a function for the square to listen when the mouse hovers over it
-// a function to allow the slider to go all the way up to 50
-// a function for if the mouse hovers over the square, then the color changes
+// a function to call the grid size
+// an eventlistener for what button is being clicked which will determine color
+// an eventlistener for if the mouse hovers over the square, then the color changes
 // a function for when the reset button is clicked
 
-slider.oninput = function() {
-    output.innerHTML = this.value;
+let color = 'black';
+
+
+function populateBoard(size) {
+  const container = document.querySelector('.container');
+  let squares = container.querySelectorAll('div');
+  squares.forEach((div) => div.remove());
+  container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+  let amount = size * size
+
+  for(let i = 0 ; i< amount; i++) {
+    let square = document.createElement('div');
+    square.addEventListener('mouseover', colorSquare);
+    square.style.backgroundColor = 'white';
+    container.insertAdjacentElement("beforeend", square);
+  }
 }
 
-function createGrid() {
+populateBoard(16);
 
+function changeSize(input) {
+    if(input >=2 && input <= 100)
+    populateBoard(input);
+    else {
+        alert('Please select a number between 2 and 100.')
+    }
 }
+
+function colorSquare() {
+    if(color === 'random') {
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    } else {
+        this.style.backgroundColor = color;
+    }
+}
+
+function changeColor(choice) {
+    color = choice;
+}
+
+function reset() {
+  const container = document.querySelector('.container');
+  let squares = container.querySelectorAll('div');
+  squares.forEach((div) => div.style.backgroundColor='white');
+}
+
+
 
